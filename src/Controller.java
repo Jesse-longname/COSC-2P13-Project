5bcea2ec42965599f7921bcf5afe7dccca25b1c8
Jesse-Daniel Belleau-Karaskewich <jb15iq@brocku.ca>
2018-05-23 15:28:21 -0400

    Cleaned up code

--- src/Controller.java
+++ src/Controller.java
@@ -1,39 +1,48 @@
-import java.util.ArrayList;
-import java.util.concurrent.Semaphore;
-
+/**
+ * Sets up multiple PiCalculators and has any control variables used by the PiCalculators.
+ * Has a method to calculate the value of Pi.
+ */
 public class Controller {
     long numElements;
     int numThreads;
+
     PiCalculator[] calculators;
     Thread[] threads;
-    ArrayList<Integer> acquired;
-    Object[] controlVariables;
+    Object[] controlVariables; // Using general Object, because it is only there to be locked.
 
+    /**
+     * Creates the controller class and does basic setup for the threads and calculators.
+     * @param numThreads The number of threads to use.
+     * @param numElements How many elements of the Pi equation to calculate in total.
+     */
     public Controller(int numThreads, long numElements) {
         this.numElements = numElements;
         this.numThreads = numThreads;
+
         // Set up threads
         calculators = new PiCalculator[numThreads];
         threads = new Thread[numThreads];
+        // Set up the control variables
         controlVariables = new Object[numThreads];
         for (int i = 0; i < numThreads; i++) {
             controlVariables[i] = new Object();
         }
-
-        // Debugging
-        acquired = new ArrayList<>();
     }
 
+    /**
+     * Starts of the PiCalculators, waits for them to finish, and returns to cumulative value that they calculated.
+     * @return The value of Pi
+     */
     public double doCalculation() {
         // Set up all but the last thread, since it will have the remaining elements.
         long elementsPerThread = numElements / numThreads;
         for (int i = 0; i < numThreads-1; i++) {
-            calculators[i] = new PiCalculator(this, i, i*elementsPerThread, ((i+1)*elementsPerThread));
+            calculators[i] = new PiCalculator(controlVariables, i, i*elementsPerThread, ((i+1)*elementsPerThread));
             threads[i] = new Thread(calculators[i]);
         }
 
         // Set up final thread with the remaining elements
-        calculators[numThreads - 1] = new PiCalculator(this,numThreads-1, (numThreads-1)*elementsPerThread, numElements);
+        calculators[numThreads - 1] = new PiCalculator(controlVariables,numThreads-1, (numThreads-1)*elementsPerThread, numElements);
         threads[numThreads - 1] = new Thread(calculators[numThreads - 1]);
 
         // Start all of the Threads
@@ -56,14 +65,5 @@ public class Controller {
             total += calculators[i].calculated;
         }
         return total*4;
-    }
-
-    public void doAction() {
-        calculate((int)(Math.random() * 4 + 36));
-    }
-
-    private static long calculate(int n) {
-        if (n <= 1) return n;
-        else return calculate(n-1) + calculate(n-2);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,20 @@
 import java.util.Scanner;
 
 /**
- * Calculates Pi using the formula given in the assignment. Asks for a number of
- *  elements to calculate, then asks for a number of Threads to use.
- * After a;; Threads are finished, prints the calculated value to the console.
+ * Asks the user to enter some runtime variables, and passes them to the Controller to start running everything.
  */
 public class Main {
-
-
+    public static boolean DEBUG_MODE = false;
 
     public static void main(String[] args) {
-        for (int i = 0; i < 100; i++) {
-            Scanner s = new Scanner(System.in);
-            System.out.println("How many elements to calculate?");
-//        long numElements = s.nextLong();
-            long numElements = 100000;
-            System.out.println("How many threads to use?");
-//        int numThreads = s.nextInt();
-            int numThreads = 10;
+        Scanner s = new Scanner(System.in);
+        System.out.println("How many elements to calculate?");
+        long numElements = s.nextLong();
+        System.out.println("How many threads to use?");
+        int numThreads = s.nextInt();
 
-            Controller controller = new Controller(numThreads, numElements);
-            double result = controller.doCalculation();
-            System.out.println("The calculated value for Pi is: " + result);
-        }
+        Controller controller = new Controller(numThreads, numElements);
+        double result = controller.doCalculation();
+        System.out.println("The calculated value for Pi is: " + result);
     }
 }
